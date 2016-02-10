01ab29131268a2836ff6df212c289bd4a03c37f1
David Elliott <david@philosophie.is>
2016-02-10 12:37:09 -0800

    remove failing test

--- spec/heroku_env_compare_spec.rb
+++ spec/heroku_env_compare_spec.rb
@@ -4,8 +4,4 @@ describe HerokuEnvCompare do
   it 'has a version number' do
     expect(HerokuEnvCompare::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end

dc9634706bddd1e2c67f779134ddb51f177765a3
David Elliott <david@philosophie.is>
2016-02-10 12:48:43 -0800

    remove explicit namespace

--- lib/heroku_env_compare.rb
+++ lib/heroku_env_compare.rb
@@ -5,8 +5,8 @@ require 'set'
 module HerokuEnvCompare
   class KeyDiff
     def self.compare(app_a, app_b)
-      a_keys = Set.new(JSON.parse(HerokuEnvCompare::Heroku.config(app_a)).keys)
-      b_keys = Set.new(JSON.parse(HerokuEnvCompare::Heroku.config(app_b)).keys)
+      a_keys = Set.new(JSON.parse(Heroku.config(app_a)).keys)
+      b_keys = Set.new(JSON.parse(Heroku.config(app_b)).keys)
       (a_keys ^ b_keys).to_a
     end
   end
